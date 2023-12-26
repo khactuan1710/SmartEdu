a47139323b7e0ef4614cc7cb859c1e16c3d0124c
NguyenKhacTuan <@tts.tuannk>
2023-12-26 15:14:50 +0700

    fix bugs

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const PORT = 3000;
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../homePage/HomePage.html'));
+    res.sendFile(path.join(__dirname, './homePage/HomePage.html'));
 });
 app.get('/SubKeywordsPage', function (req, res) {
-    res.sendFile(path.join(__dirname, '../subKeywordsPage/SubKeywordsPage.html'));
+    res.sendFile(path.join(__dirname, './subKeywordsPage/SubKeywordsPage.html'));
 });
 app.get('/RoadmapPage', function (req, res) {
-    res.sendFile(path.join(__dirname, '../roadmapPage/RoadmapPage.html'));
+    res.sendFile(path.join(__dirname, './roadmapPage/RoadmapPage.html'));
 });
 app.get('/RecommenderSystemPage', function (req, res) {
-    res.sendFile(path.join(__dirname, '../rcmSysPage/RecommenderSystemPage.html'));
+    res.sendFile(path.join(__dirname, './rcmSysPage/RecommenderSystemPage.html'));
 });
 app.get('/WatchVideos', function (req, res) {
-    res.sendFile(path.join(__dirname, '../aiPlaylistAndVideosPage/AiPlaylistAndVideosPage.html'));
+    res.sendFile(path.join(__dirname, './aiPlaylistAndVideosPage/AiPlaylistAndVideosPage.html'));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
